281cd45ea3018133c834221188a7f199d194f7e0
Semjon Molokanov <semjon.molokanov@mynewsdesk.com>
2017-01-04 21:42:28 +0100

    Implement StringRenderer of Canvas points map

---
+++ lib/render/string_renderer.rb
@@ -0,0 +1,32 @@
+module ConsoleDraw
+  module Render
+
+    # Class: Renders points map as string
+    class StringRenderer
+      class << self
+        TRANSPARENT_CHAR = ' '.freeze
+        VISIBLE_CHAR = 'x'.freeze
+
+        # Public: Iterates over points on map and collect string output. Same as "Raster scan"
+        #   points_map - 2-dimensional array with points
+        #
+        # Example:
+        #   ConsoleDraw::Render::StringRenderer.render([[true, nil], [nil, true]])
+        #   # => "x \n x"
+        #
+        # Returns: String representation of points_map
+        def render(points_map)
+          points_map.map do |row|
+            row.map { |point| render_point(point) }.join
+          end.join("\n")
+        end
+
+        private
+
+        def render_point(point)
+          point.nil? ? TRANSPARENT_CHAR : VISIBLE_CHAR
+        end
+      end
+    end
+  end
+end
---
+++ spec/render/string_renderer_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../../lib/render/string_renderer'
+
+describe ConsoleDraw::Render::StringRenderer do
+  describe '.render' do
+    subject { described_class.render(points_map) }
+
+    context 'when empty map' do
+      let(:points_map) { [[]] }
+
+      it 'renders empty string' do
+        expect(subject).to eq ''
+      end
+    end
+
+    context 'when map without points' do
+      let(:points_map) { [[nil], [nil]] }
+
+      it 'renders spaces and new lines' do
+        expect(subject).to eq " \n "
+      end
+    end
+
+    context 'when map with points and holes' do
+      let(:points_map) { [[nil, true], [true, nil]] }
+
+      it 'renders visible chars and spaces' do
+        expect(subject).to eq " x\nx "
+      end
+    end
+  end
+end
